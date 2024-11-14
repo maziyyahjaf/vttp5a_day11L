4b0af35e6be3312f0fcde4bb5902771819522414
maziyyahjaf <mazi3ah@gmail.com>
2024-11-14 13:43:52 +0800

    second commit

---
+++ src/main/java/sg/edu/nus/iss/vttp5a_day11l/controller/HomeController.java
@@ -0,0 +1,33 @@
+package sg.edu.nus.iss.vttp5a_day11l.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @ResponseBody
+    @RequestMapping(path="/log", method=RequestMethod.GET)
+    public String home() {
+
+        logger.info("This is an info message");
+        logger.warn("This is a warning message");
+        logger.error("This is an error message");
+        logger.debug("This is a debug message");
+        logger.trace("This is a trace message");
+
+        return "";
+    }
+
+    @RequestMapping(path="/home", method=RequestMethod.GET)
+    public String homePage() {
+        return "home.html";
+    }
+    
+}
